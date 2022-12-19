16c73a136d0c9484b0c8b4e1c05866d6ed3cd311
Leandro <leandro.figueiredo14848@gmail.com>
2022-12-19 13:31:05 -0500

    added migrations and model files

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -36,3 +36,10 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+//migration commands
+// npx sequelize-cli model:generate --name Booking --attributes spotId:integer,userId:integer,startDate:date,endDate:date
+// npx sequelize-cli model:generate --name Spot --attributes ownerId:integer,address:string,city:string,state:string,country:string,lat:decimal,lng:decimal,name:string,description:string,price:decimal
+// npx sequelize-cli model:generate --name Review --attributes spotId:integer,userId:integer,review:string,stars:integer
+// npx sequelize-cli model:generate --name SpotImage --attributes spotId:integer,url:string,preview:boolean
+// npx sequelize-cli model:generate --name ReviewImage --attributes reviewId:integer,url:string
