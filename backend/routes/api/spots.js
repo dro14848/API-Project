9a8d6d484c6a6a23260ab66909ef3d300b62bcfd
Leandro <leandro.figueiredo14848@gmail.com>
2022-12-22 22:24:04 -0500

    bug test

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -22,6 +22,7 @@ router.get('/', async(req, res) => {
                 model: SpotImage
             }
         ],
+        group: ["Spot.id", "Reviews.id", "SpotImages.id"]
     })
 
     let allspots = [];
