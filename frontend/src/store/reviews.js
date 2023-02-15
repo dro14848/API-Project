bad09ec5c36b9a0fca7fb688f14eb9d2638678b7
Leandro <leandro.figueiredo14848@gmail.com>
2023-02-15 18:38:37 -0500

    initial reducer file

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,8 @@
+import { csrfFetch } from "./csrf";
+
+/* create read delete */
+
+const CREATE_REVIEW = "reviews/create"
+const LOAD_REVIEW = "reviews/create"
+const DELETE_REVIEW = "reviews/delete"
+
