c335a9fcbb18eef7eab52cd12a55b48e5b960ffc
Leandro <leandro.figueiredo14848@gmail.com>
2023-02-15 19:10:02 -0500

    Broken Modal

--- frontend/src/components/Spot-Edit/index.js
+++ frontend/src/components/Spot-Edit/index.js
@@ -1,33 +1,16 @@
-import { useEffect, useState} from "react";
-import { useParams, useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useModal } from "../../context/Modal";
-import { updateSpotThunk } from "../../store/spot";
+import { useState } from "react"
 
 function EditSpot () {
-    const dispatch = useDispatch();
-    const { closeModal } = useModal();
-    const spot = useSelector((state) => state.Spots.singleSpot)
-    // const userSession = useSelector((state) => state.session.user)
-
-    const [address, setAddress] = useState(spot.address);
-    const [city, setCity] = useState(spot.city);
-    const [state, setState] = useState(spot.state);
-    const [country, setCountry] = useState(spot.country);
-    const [name, setName] = useState(spot.name);
-    const [description, setDescription] = useState(spot.description);
-    const [price, setPrice] = useState(spot.price)
-    const [errors, setErrors] = useState([]);
+    console.log(useState)
     
-    console.log("SPOTCHECK", spot)
+    const [name, setName] = useState("")
 
+return (
+    <div>
+        <h1>TEST TEST </h1>
+    </div>
+)
 
-    return (
-      <div>
-        EDIT SPOT TEST
-      </div>  
-    )
-    }
-
+}
 
 export default EditSpot
--- frontend/src/components/Spot-Single/index.js
+++ frontend/src/components/Spot-Single/index.js
@@ -2,7 +2,7 @@ import { useEffect} from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteSpotThunk, singleSpotThunk } from "../../store/spot";
-import OpenModalMenuItem from "../Navigation/OpenModalMenuItem"
+import OpenModalButton from "../OpenModalButton";
 import { useModal } from "../../context/Modal";
 import  EditSpot from '../Spot-Edit'
 
@@ -28,10 +28,9 @@ function SpotSingle() {
         <div className="spotdetails">
             <h1 className="name">{singleSpot.name}</h1>
            <div className="Edit-button">
-             <OpenModalMenuItem
-            className='Edit-Button'
-            itemText={"Edit Spot"}
-            modalComponent={EditSpot}
+           <OpenModalButton 
+        modalComponent={EditSpot}
+        buttonText={"BUTTON TEST "}
             />
     
            </div>
@@ -43,6 +42,7 @@ function SpotSingle() {
             >
             Delete This Spot
             </button> 
+            <EditSpot />
             <div className="ratingline">
             <p className="avgRatinginDetails">{singleSpot.avgStarRating}</p>
             <p className="address">{singleSpot.address}, {singleSpot.city}, {singleSpot.state}, {singleSpot.country}</p>
